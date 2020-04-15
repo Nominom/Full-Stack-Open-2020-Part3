4fa8d5f6f6d8e1f2f3154cc8c8c3bed9a234e8bd
Nominom <nomi.lakkala@gmail.com>
2020-04-15 15:40:25 +0300

    3.13

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.json())
@@ -11,61 +13,37 @@ morgan.token('json', (req) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
 app.use(express.static('build'))
 
-let persons = [
-	{
-		name: "Arto Hellas",
-		number: "040-123456",
-		id: 1
-	},
-	{
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-		id: 2
-	},
-	{
-		name: "Dan Abramov",
-		number: "12-43-234345",
-		id: 3
-	},
-	{
-		name: "Mary Poppendieck",
-		number: "39-23-6423122",
-		id: 4
-	}
-]
-
-app.get('/', (req, res) => {
-	res.send('<h1>Phonebook</h1>')
-})
-
 app.get('/info', (req, res) => {
-	res.send(`
-	<p>Phonebook has info for ${persons.length} people</p> 
-	<p>${new Date()}</p>`)
+	Person.find({}).then(persons => {
+		res.send(`
+		<p>Phonebook has info for ${persons.length} people</p> 
+		<p>${new Date()}</p>`)
+	})
 })
 
 app.get('/api/persons/:id', (request, response) => {
 	console.log(request.params.id)
-	const id = Number(request.params.id)
-	const person = persons.find(person => person.id === id)
-
-	if (person) {
-		response.json(person)
-	} else {
-		response.status(404).end()
-	}
+	Person.findById(request.params.id).then(person => {
+		if (person) {
+			response.json(person.toJSON())
+		} else {
+			response.status(404).end()
+		}
+	})
 })
 
 app.get('/api/persons', (req, res) => {
-	res.json(persons)
+	Person.find({}).then(persons => {
+		res.json(persons)
+	})
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	persons = persons.filter(person => person.id !== id)
+// app.delete('/api/persons/:id', (request, response) => {
+// 	const id = Number(request.params.id)
+// 	persons = persons.filter(person => person.id !== id)
 
-	response.status(204).end()
-})
+// 	response.status(204).end()
+// })
 
 const generateId = () => Math.floor(Math.random() * 9999999)
 
